196947d117978efc5ecbae17828b9b633ea7c6ff
Emma Dauterman <evd2014@gmail.com>
2018-05-26 19:21:35 +0000

    change sender addr for encapsulated packets

--- lib/udptransport.cc
+++ lib/udptransport.cc
@@ -744,6 +744,10 @@ UDPTransport::ProcessPacket(int fd, sockaddr_in sender, socklen_t senderSize,
 	    Notice("Found encapsulated packet");
             DecodePacket(buf + sizeof(uint32_t), sz - sizeof(uint32_t),
                      msgType, msg, &meta_data);
+	    struct iphdr *iph = (struct iphdr *)(buf + sizeof(struct ether_header));
+	    struct udphdr *udph = (struct udphdr *)(buf + sizeof(struct iphdr));
+	    sender.sin_port = iph->saddr;
+            sender.sin_addr.s_addr = udph->source;
 	} else { 
 	    Warning("Received packet with bad magic number");
 	}
