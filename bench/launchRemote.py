1b1d5f371e50a60c5f0e9b51b4ec73110eb3d21b
Zoë Bohn <zbohn2@DN0a1f49e8.SUNet>
2018-05-27 01:12:52 -0700

    fixed script lol

--- bench/launchRemote.py
+++ bench/launchRemote.py
@@ -21,21 +21,26 @@ tot_throughput = 0.0
 tot_latency = 0.0
 
 # Start all clients, non-blocking
+processes = []
 for machine in machines:
-    cmd = subprocess.Popen(generateCmdStr(machine), shell=True)
+    process = subprocess.Popen(generateCmdStr(machine), shell=True, stdout=subprocess.PIPE)
+    processes.append(process)
 
 # Wait for output
-for machine in machines:
-    output = subprocess.check_output(generateCmdStr(machine), shell=True)
+for i, machine in enumerate(machines):
+    output = processes[i].stdout.read() 
     outputLines = output.splitlines()
     elems = outputLines[0].split(":")
+    print elems
+    print ("Adding value to throughput: " + str(float(elems[1])))
     tot_throughput += float(elems[1])
     elems = outputLines[1].split(":")
+    print ("Adding value to latency: " + str(float(elems[1])))
     tot_latency += float(elems[1])
 
 # Collect output
-avg_throughput = tot_throughput / len(machines)
+#avg_throughput = tot_throughput / len(machines)
 avg_latency = tot_latency / len(machines)
 
-print ("Average throughput across %d machines each with %d threads running %s: %d") % (len(machines), threads, protocol, avg_throughput)
+print ("Total throughput across %d machines each with %d threads running %s: %d") % (len(machines), threads, protocol, tot_throughput)
 print ("Average latency across %d machines each with %d threads running %s: %d") % (len(machines), threads, protocol, avg_latency)
