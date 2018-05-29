d9db0431b2509f96e38e5ddaba03943e15f2eb6d
Zoë Bohn <zbohn2@DN0a1f49e8.SUNet>
2018-05-29 15:23:09 -0700

    changes to launch script to make it work

--- bench/launchRemote.py
+++ bench/launchRemote.py
@@ -16,7 +16,7 @@ machines = sys.argv[3:]
 project = "nopaxos-204404"
 zone = "us-west1-a"
 config = "config-5"
-remote_cmd = ("cd NOPaxos; rm output.txt; ./bench/client -t %d -c %s -m %s &> output.txt; python ./bench/combineThreadOutputs.py") % (threads, config, protocol)
+remote_cmd = ("cd ../emmadauterman/NOPaxos; rm output.txt; ./bench/client -t %d -c %s -n 1000 -m %s &> output.txt; python ./bench/combineThreadOutputs.py") % (threads, config, protocol)
 tot_throughput = 0.0
 tot_latency = 0.0
 
